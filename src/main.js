11ee169f62b4731015a20bc592da2c42565b84e2
Chris Bacon <chrisbacon2009@hotmail.com>
2023-02-20 10:05:31 +0000

    refactor

--- src/main.js
+++ src/main.js
@@ -1,25 +1,28 @@
 import { google } from 'googleapis';
 import credentials from '../credentials.json' assert { type: 'json' };
 
-async function main() {
-  const sheets = google.sheets({ version: 'v4', auth: credentials.api_key });
-  const res = await sheets.spreadsheets.values.get({
-    spreadsheetId: '1VJI0G67jWe4KFeDyqrUpId1pX1-iK0A16maJ7I_pqP4',
-    range: 'Deposits Stream!A2:B',
-  });
-  const rows = res.data.values;
+const rowMap = {
+  "Options": (acc, next) => ({ ...acc, optionsDeposits: acc.optionsDeposits + parseInt(next[1]) }),
+  "ISA": (acc, next) => ({ ...acc, isaDeposits: acc.isaDeposits + parseInt(next[1]) }),
+};
+
+const main = async () => {
+  const { data: { values: rows } } = await google
+    .sheets({ version: 'v4', auth: credentials.api_key })
+    .spreadsheets
+    .values
+    .get({
+      spreadsheetId: '1VJI0G67jWe4KFeDyqrUpId1pX1-iK0A16maJ7I_pqP4',
+      range: 'Deposits Stream!A2:B',
+    });
+
   if (!rows || rows.length === 0) {
     console.log('No data found.');
     return;
   }
 
-  const rowMap = {
-    "Options": (acc, next) => ({ ...acc, optionsDeposits: acc.optionsDeposits + parseInt(next[1]) }),
-    "ISA":     (acc, next) => ({ ...acc, isaDeposits: acc.optionsDeposits + parseInt(next[1]) }),
-  };
-
   const summedRows = rows.reduce((acc, next) => {
-    const rowEntry = rowMap[next];
+    const rowEntry = rowMap[next[0]];
 
     return rowEntry ? rowEntry(acc, next) : acc;
   }, { "optionsDeposits": 0, "isaDeposits": 0 });
