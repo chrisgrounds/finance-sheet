94e43d3e5893ab01491de17ceafcdc0efacf2975
Chris Bacon <chrisbacon2009@hotmail.com>
2023-02-20 12:17:40 +0000

    Add JWT Auth and write to sheet

--- src/main.js
+++ src/main.js
@@ -1,14 +1,40 @@
 import { google } from 'googleapis';
-import credentials from '../credentials.json' assert { type: 'json' };
+import privKey from '../priv_key.json' assert { type: 'json' };
 
 const rowMap = {
   "Options": (acc, next) => ({ ...acc, optionsDeposits: acc.optionsDeposits + parseInt(next[1]) }),
   "ISA": (acc, next) => ({ ...acc, isaDeposits: acc.isaDeposits + parseInt(next[1]) }),
 };
 
+const auth = async () => {
+  const jwtClient = new google.auth.JWT(
+    privKey.client_email,
+    null,
+    privKey.private_key,
+    [
+      "https://www.googleapis.com/auth/spreadsheets",
+    ]
+  );
+
+  return new Promise((resolve, reject) => {
+    jwtClient.authorize(function (err, tokens) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        console.log("Success authenticating...")
+        resolve(jwtClient);
+      }
+    });
+  });
+}
+
 const main = async () => {
-  const { data: { values: rows } } = await google
-    .sheets({ version: 'v4', auth: credentials.api_key })
+  const jwtClient = await auth();
+
+  const sheets = google.sheets({ version: 'v4', auth: jwtClient });
+
+  const { data: { values: rows } } = await sheets
     .spreadsheets
     .values
     .get({
@@ -28,6 +54,39 @@ const main = async () => {
   }, { "optionsDeposits": 0, "isaDeposits": 0 });
 
   console.log(summedRows);
+
+  const dataToWrite = {
+    values: [
+      [
+        "Total Options Deposits",
+        "Total ISA Deposits"
+      ],
+      [
+        summedRows.optionsDeposits,
+        summedRows.isaDeposits
+      ]
+    ]
+  }
+
+  sheets
+    .spreadsheets
+    .values
+    .append({
+      spreadsheetId: '1VJI0G67jWe4KFeDyqrUpId1pX1-iK0A16maJ7I_pqP4',
+      range: 'Deposits Stream!G1',
+      resource: dataToWrite,
+      valueInputOption: "RAW",
+    }, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(
+          '%d cells updated on range: %s',
+          result.data.updates.updatedCells,
+          result.data.updates.updatedRange
+        );
+      }
+    });
 }
 
 main();
