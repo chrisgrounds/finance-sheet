5cdab4901d89874532cd20fef67ce3b0d9902f4f
Chris Bacon <chrisbacon2009@hotmail.com>
2023-02-20 15:59:43 +0000

    prettier

--- src/auth.js
+++ src/auth.js
@@ -2,24 +2,24 @@ import { google } from "googleapis";
 import { Log } from "./Log.js";
 
 export const auth = async (privKey) => {
-    const jwtClient = new google.auth.JWT(
-        privKey.client_email,
-        null,
-        privKey.private_key,
-        [
-            "https://www.googleapis.com/auth/spreadsheets",
-        ]
-    );
+  const jwtClient = new google.auth.JWT(
+    privKey.client_email,
+    null,
+    privKey.private_key,
+    [
+      "https://www.googleapis.com/auth/spreadsheets",
+    ]
+  );
 
-    return new Promise((resolve, reject) => {
-        jwtClient.authorize(function (err, tokens) {
-            if (err) {
-                Log.error(err);
-                reject(err);
-            } else {
-                Log.info("Success authenticating...")
-                resolve(jwtClient);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    jwtClient.authorize(function (err, tokens) {
+      if (err) {
+        Log.error(err);
+        reject(err);
+      } else {
+        Log.info("Success authenticating...")
+        resolve(jwtClient);
+      }
     });
+  });
 }
--- src/sheets.js
+++ src/sheets.js
@@ -1,46 +1,47 @@
-import { auth } from "./auth.js";
 import { google } from "googleapis";
+import { auth } from "./auth.js";
+import { Log } from "./Log.js";
 
 export async function loadSheets(privKey, sheetId) {
-    const jwtClient = await auth(privKey);
-    const sheets = await google.sheets({ version: 'v4', auth: jwtClient });
-    return {
-        loadRows: async (depositRangeInput) => {
-            const { data: { values: rows } } = await sheets
-                .spreadsheets
-                .values
-                .get({
-                    spreadsheetId: sheetId,
-                    range: depositRangeInput,
-                });
-            return rows;
-        },
-        writeRows: async (depositRangeOutput, deposits) => {
-            const dataToWrite = {
-                values: [
-                    Object.keys(deposits).map(type => `Total ${type} Deposits`),
-                    Object.values(deposits),
-                ]
-            }
-            await sheets
-                .spreadsheets
-                .values
-                .append({
-                    spreadsheetId: sheetId,
-                    range: depositRangeOutput,
-                    resource: dataToWrite,
-                    valueInputOption: "RAW",
-                }, (err, result) => {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log(
-                            '%d cells updated on range: %s',
-                            result.data.updates.updatedCells,
-                            result.data.updates.updatedRange
-                        );
-                    }
-                });
-        }
+  const jwtClient = await auth(privKey);
+  const sheets = await google.sheets({ version: 'v4', auth: jwtClient });
+
+  return {
+    loadRows: async (depositRangeInput) => {
+      const { data: { values: rows } } = await sheets
+        .spreadsheets
+        .values
+        .get({
+          spreadsheetId: sheetId,
+          range: depositRangeInput,
+        });
+
+      return rows;
+    },
+    writeRows: async (depositRangeOutput, deposits) => {
+      const dataToWrite = {
+        values: [
+          Object.keys(deposits).map(type => `Total ${type} Deposits`),
+          Object.values(deposits),
+        ]
+      }
+      await sheets
+        .spreadsheets
+        .values
+        .append({
+          spreadsheetId: sheetId,
+          range: depositRangeOutput,
+          resource: dataToWrite,
+          valueInputOption: "RAW",
+        }, (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            Log.info(
+              `${result.data.updates.updatedCells} cells updated on range: ${result.data.updates.updatedRange}`
+            );
+          }
+        });
     }
+  }
 }
