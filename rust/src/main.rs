36a2a51b9fa5a73bf72c988387d134509f4fd36f
Chris Bacon <chrisbacon2009@hotmail.com>
2023-02-23 12:42:43 +0000

    Move sheets read function to own file

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,20 +5,16 @@ use std::collections::HashMap;
 mod auth;
 mod config;
 mod http_client;
+mod sheets;
 
 #[tokio::main]
 async fn main() {
     let config = config::Config::new();
     let client = http_client::http_client();
     let auth = auth::auth(&config, client.clone()).await;
-
     let mut hub = Sheets::new(client.clone(), auth);
 
-    let result = hub
-        .spreadsheets()
-        .values_get(&config.sheet_id, &config.deposit_range_input)
-        .doit()
-        .await;
+    let result = sheets::read(&hub, &config).await;
 
     match result {
         Err(e) => println!("{}", e),
